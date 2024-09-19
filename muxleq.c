2921aa5716b051d5e94a4740b6b560f69e95db8f
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-09-19 23:15:54 +0800

    Refine the sentinel value
    A sentinel value is typically used to indicate a special condition. The
    actual value of the constant (-1), which is useful if the constant is
    specifically meant to represent the marker.

--- muxleq.c
+++ muxleq.c
@@ -1,10 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static const uint16_t n = -1;
-
 #define SZ (1 << 15)
 #define MASK (SZ - 1)
+#define MARKER (uint16_t)(~0U)
 
 static uint16_t m[SZ] = {
 #include "muxleq-dec.c"
@@ -17,17 +16,17 @@ int main()
 
     for (uint16_t pc = 0; pc < SZ;) {
         uint16_t a = m[pc++], b = m[pc++], c = m[pc++];
-        if (a == n) {
+        if (a == MARKER) {
             m[b] = getchar();
-        } else if (b == n) {
+        } else if (b == MARKER) {
             if (putchar(m[a]) < 0)
                 return 3;
-        } else if (c & SZ && c != n) {
+        } else if (c & SZ && c != MARKER) {
             uint16_t mc = m[c & MASK];
             m[b] = (m[a] & ~mc) | (m[b] & mc);
         } else {
             uint16_t r = m[b] - m[a];
-            if (r == 0 || r & 32768)
+            if (r == 0 || r & SZ)
                 pc = c;
             m[b] = r;
         }
