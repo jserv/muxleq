0992460b4ee9cca6bf9a1b501034caef254c6958
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-09-20 09:17:26 +0800

    Add comments

--- muxleq.c
+++ muxleq.c
@@ -1,32 +1,47 @@
+/*
+ * A MUXLEQ virtual machine implementation.
+ *
+ * This program reads instructions from a memory array and executes them.
+ * It supports input, output, MUX operations, and SUBLEQ (Subtract and Branch
+ * if Less or Equal to Zero) instructions.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 
 #define SZ (1 << 15)
 #define MASK (SZ - 1)
-#define MARKER (uint16_t)(~0U)
+#define IO_MARKER (uint16_t) (~0U)
 
+/* the memory array of the virtual machine */
 static uint16_t m[SZ] = {
 #include "muxleq-dec.c"
 };
 
 int main()
 {
+    /* Disable buffering for stdout to ensure immediate output. */
     if (setvbuf(stdout, NULL, _IONBF, 0) < 0)
         return 1;
 
     for (uint16_t pc = 0; pc < SZ;) {
+        /* instruction operands */
         uint16_t a = m[pc++], b = m[pc++], c = m[pc++];
-        if (a == MARKER) {
+        if (a == IO_MARKER) { /* Read a byte from input and store in memory */
             m[b] = getchar();
-        } else if (b == MARKER) {
-            if (putchar(m[a]) < 0)
+        } else if (b == IO_MARKER) {
+            if (putchar(m[a]) < 0) /* Write the byte from memory to output */
                 return 3;
-        } else if (c & SZ && c != MARKER) {
-            uint16_t mc = m[c & MASK];
+        } else if (c & SZ && c != IO_MARKER) {
+            /* Handles MUX operations by performing a bitwise multiplexing */
+            uint16_t mc = m[c & MASK]; /* mux condition */
             m[b] = (m[a] & ~mc) | (m[b] & mc);
         } else {
+            /* Handles SUBLEQ operations by subtracting one memory value from
+             * another and conditionally branching.
+             */
             uint16_t r = m[b] - m[a];
-            if (r == 0 || r & SZ)
+            if (r == 0 || r & SZ) /* Check if it is zero or negative */
                 pc = c;
             m[b] = r;
         }
